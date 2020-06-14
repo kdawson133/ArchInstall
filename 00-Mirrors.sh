7ae52a5587cd3ca735854f0b71dac71a5d90f223
kdawson133 <kdawson133@gmail.com>
2020-06-14 21:38:37 +1000

    Update 00-Mirrors.sh

--- 00-Mirrors.sh
+++ 00-Mirrors.sh
@@ -7,7 +7,7 @@ pacman -Syyy
 # Install Refector
 pacman -S reflector --noconfirm --needed
 # backup mirrorlist
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 # run reflector
 reflector -c Australia 6 --sort rate --save /etc/pacman.d/mirrorlist
 # resynchronise repositories

d6c90c2d87f2ac2072674abb3f8770d8058c54fb
kdawson133 <kdawson133@gmail.com>
2020-06-14 15:31:43 +1000

    Update 50-Audio.sh, 60-Printers.sh, and 4 more files...

---
+++ 60-Printers.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+echo
+echo "== Installing Printer Drivers =="
+echo
+
+PKGS=(
+    'cups'                  # Open source printer drivers
+    'cups-pdf'              # PDF support for cups
+    'ghostscript'           # PostScript interpreter
+    'gsfonts'               # Adobe Postscript replacement fonts
+#    'hplip'                 # HP Drivers
+    'system-config-printer' # Printer setup  utility
+)
+
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+echo
+echo "== Printer Drivers Installed =="
+echo
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ArchInstall
 Arch Install Guide and Scripts
+
+

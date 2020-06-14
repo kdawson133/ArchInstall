ce53891d2cdaafa8564c8bf6561e98d30d15570f
kdawson133 <kdawson133@gmail.com>
2020-06-14 15:20:23 +1000

    Update 30-Network and 30-Network.sh

---
+++ 30-Network.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo
+echo "== Installing Network Components =="
+echo
+
+PKGS=(
+        'wpa_supplicant'            # Key negotiation for WPA wireless networks
+        'wireless_tools'			# Wireless tools
+        'dialog'                    # Enables shell scripts to trigger dialog boxex
+        'networkmanager'            # Network connection manager
+        'network-manager-applet'    # System tray icon/utility for network connectivity
+)
+
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING: ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+echo
+echo "== Network Components Installed =="
+echo

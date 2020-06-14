6c6655f2b723cfd19492995e3feb018c785b57be
kdawson133 <kdawson133@gmail.com>
2020-06-14 14:53:23 +1000

    Update 00-SetupMirrors.sh and 10-DisplayServer.sh

--- 00-SetupMirrors.sh
+++ 00-SetupMirrors.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
+echo
+echo "== Configuring Repository Mirrors =="
+echo
 # Synchronise pacman repositories
 pacman -Syyy
 # Install Refector
-pacman -S reflector
+pacman -S reflector --noconfirm --needed
 # backup mirrorlist
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 # run reflector
 reflector -c Australia 6 --sort rate --save /etc/pacman.d/mirrorlist
 # resynchronise repositories
+pacman -Syyy
+echo
+echo "== Repository Mirrors congigured =="
+echo
+
+

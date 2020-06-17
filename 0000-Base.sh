e5467b5f9e70f5ff1a8065f6822976343e7b0c84
Kirk Dawson <ktdawson@neon>
2020-06-18 08:54:31 +1000

    Update 0000-Base.sh

--- 0000-Base.sh
+++ 0000-Base.sh
@@ -5,20 +5,41 @@
 keymap=us
 country=Australia
 
+bootpart=/dev/sda1
+swappart=/dev/sda2
+rootpart=/dev/sda3
+
 # Load the keymap
+
 loadkeys=$keymap
 
 # Enable NTP
+
 timedatectl set-ntp true
 
 # Configure the mirrorlist
+
 pacman -Syyy
 pacman -S reflector --noconfirm
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 reflector -c $country -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syyy
 
-# Partitioning Disks
+# Formatting Partitions
+
+mkfs.ext4 $rootpart
+mkswap $swappart
+
+# Mount Partitions
+
+swapon $swappart
+mount $rootpart /mnt
+mkdir /mnt/boot
+mount $bootpart /mnt/boot
+
+# Install Base
+
+
 
 
 

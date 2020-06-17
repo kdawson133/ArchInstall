51cef94ac433d3764303338ec0d71862ea4bc901
Kirk Dawson <ktdawson@neon>
2020-06-18 08:26:04 +1000

    Update 0000-Base.sh

--- 0000-Base.sh
+++ 0000-Base.sh
@@ -1,1 +1,24 @@
 #!/bin/sh
+# This script installs the arch base only
+#########################################
+# Variables
+keymap=us
+country=Australia
+
+# Load the keymap
+loadkeys=$keymap
+
+# Enable NTP
+timedatectl set-ntp true
+
+# Configure the mirrorlist
+pacman -Syyy
+pacman -S reflector --noconfirm
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+reflector -c $country -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+pacman -Syyy
+
+# Partitioning Disks
+
+
+

1b898f96f63372eb3219c62880f748b42441e2b9
kdawson133 <kdawson133@gmail.com>
2020-06-18 13:13:52 +1000

    Update 0000-Base.sh

--- 0000-Base.sh
+++ 0000-Base.sh
@@ -58,59 +58,59 @@ cat /mnt/etc/fstab
 
 # Enter CHROOT Environment
 
-arch-chroot /mnt 
+# arch-chroot /mnt 
 
 # Set localisation
 
 timedatectl set-timezone $timezone
 
-hwclock --systohc --localtime
+arch-chroot /mnt hwclock --systohc --localtime
 
-echo "en_AU.UTF-8" >> /etc/locale.gen
+arch-chroot /mnt echo "en_AU.UTF-8" >> /etc/locale.gen
 
-local-gen
+arch-chroot /mnt local-gen
 
-echo en_AU.UTF-8 >> /etc/locale.Configure
-export en_AU.UTF-8
-echo KEYMAP=$keymap >> /etc/vconsole.conf
+arch-chroot /mnt echo en_AU.UTF-8 >> /etc/locale.Configure
+arch-chroot /mnt export en_AU.UTF-8
+arch-chroot /mnt echo KEYMAP=$keymap >> /etc/vconsole.conf
 
 # Set Network
 
-echo $hostname > /etc/hostname
+arch-chroot /mnt echo $hostname > /etc/hostname
 
-echo "127.0.0.1" '/t' "localhost" >> /etc/hosts
-echo "::1" '/t' '/t' "localhost" >> /etc/hosts
-echo "127.0.1.1" '\t' "$hostname.localdomain" '\t' "$hostname" >> /etc/hosts
+arch-chroot /mnt echo "127.0.0.1" '/t' "localhost" >> /etc/hosts
+arch-chroot /mnt echo "::1" '/t' '/t' "localhost" >> /etc/hosts
+arch-chroot /mnt echo "127.0.1.1" '\t' "$hostname.localdomain" '\t' "$hostname" >> /etc/hosts
 
 # Set Root Password
 
-passwd
+arch-chroot /mnt passwd
 
 # Install Bootloader
 
-pacman -S grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober mtools dosfstools base-devel linux-headers reflector git bluez bluez-utils bluez-libs pulseaudio-bluetooth cups ntfs-3g openssh avahi
+arch-chroot /mnt pacman -S grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober mtools dosfstools base-devel linux-headers reflector git bluez bluez-utils bluez-libs pulseaudio-bluetooth cups ntfs-3g openssh avahi
 
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ARCH
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ARCH
 
-grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 # Enable services
 
-systemctl enable NetworkManager
-systemctl enable bluetooth
-systemctl enable org.cups.cupsd
-systemctl enable sshd
-systemctl enable avahi-daemon
+arch-chroot /mnt arch-chroot /mnt systemctl enable NetworkManager
+arch-chroot /mnt systemctl enable bluetooth
+arch-chroot /mnt systemctl enaarch-chroot /mnt blarch-chroot /mnt e org.cups.cupsd
+arch-chroot /mnt systemctl enable sshd
+arch-chroot /mnt systemctl enable avahi-daemon
 
 # Create New User
 
-useradd -mG wheel $username
-passwd $username
-EDITOR=nano visudo
+arch-chroot /mnt useradd -mG wheel $username
+arch-chroot /mnt passwd $username
+arch-chroot /mnt EDITOR=nano visudo
 
 # Reboot
 
-exit
+#exit
 umount -a
 echo "Reboot the System Now"
 

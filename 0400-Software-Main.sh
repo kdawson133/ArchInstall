aab1b81f7d5304173938dfa63c4463a7737f67c9
kdawson133 <kdawson133@gmail.com>
2020-06-28 12:18:31 +1000

    update 0400-Software-Main.sh

--- 0400-Software-Main.sh
+++ 0400-Software-Main.sh
@@ -13,7 +13,13 @@
 echo
 echo "== Installing Software Main Repository =="
 echo
-
+echo "Installing Sublimr GPG Key...."
+curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+echo
+echo "Adding Sublime Stable Channel...."
+echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
+echo
+echo "Installing Packages...."
 PKGS=(
 
     # SYSTEM --------------------------------------------------------------
